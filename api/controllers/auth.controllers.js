aa755a4c45589d3a7f5458d1607e48401b706b65
adityakanthe10 <adityakanthe10@gmail.com>
2024-11-29 19:34:13 +0530

    begin with the backend

---
+++ api/controllers/auth.controllers.js
@@ -0,0 +1,9 @@
+export const register = (req, res) => {
+  // db operations
+};
+export const login = (req, res) => {
+  // db operations
+};
+export const logout = (req, res) => {
+  // db operations
+};
